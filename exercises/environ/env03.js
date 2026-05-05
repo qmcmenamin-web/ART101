5a51028299b89a209c09fe0f1c126f3f0975f7b0
qmcmenamin-web <qmcmenamin@gmail.com>
2026-05-05 12:17:11 -0700

    new

---
+++ exercises/environ/env03.js
@@ -0,0 +1,34 @@
+let environmentTitle = "Chrnos Academy";
+
+let environmentElements = ["sliding door", "elevator", "sky light", "hologram"];
+
+let mainEntity = {
+    name: "hologram",
+    type: "soft border",
+    mood: "helpful",
+    isMoving: false,
+    favoriteElement: environmentElements[1]
+};
+let count = 0;
+let buttonCreature = {
+    name: "",
+    species: "Time Tube",
+    
+        time: ["2125", "1917", "2001", "2012", "2046", "1984"]
+};
+$("#needy-button").click(function () {
+
+    count = count + 1;
+    let arrayPosition = count - 1;
+    let currentMood = buttonCreature.time[arrayPosition];
+    let message = "<p>You clicked me " + count + " times.</p>";
+    message = message + "<p>The current year is " + currentMood + ".</p>";
+
+message = message + "<p>I am a " + buttonCreature.species + ".</p>";
+
+    $("#output").html(message);
+    console.log(count);
+    console.log(arrayPosition);
+    console.log(currentMood);
+
+});
--- exercises/lab04.js
+++ exercises/lab04.js
@@ -1,9 +1,9 @@
 let count = 0;
 let buttonCreature = {
-    name: "Needy Button",
-    species: "interface creature",
-    favoriteFood: "clicks",
-    moods: ["sleepy", "nervous", "glowing", "dramatic", "hopeful", "confused"]
+    name: "Button McButtonface",
+    species: "website marmoset",
+    favoriteFood: "cheesy clicks",
+    moods: ["bored", "tentative", "excited", "pumped up", "partying", "confused"]
 };
 $("#needy-button").click(function () {
 
@@ -11,6 +11,10 @@ $("#needy-button").click(function () {
     let arrayPosition = count - 1;
     let currentMood = buttonCreature.moods[arrayPosition];
     let message = "<p>You clicked me " + count + " times.</p>";
+    message = message + "<p>My current mood is " + currentMood + ".</p>";
+message = message + "<p>My name is " + buttonCreature.name + ".</p>";
+message = message + "<p>I am an " + buttonCreature.species + ".</p>";
+message = message + "<p>My favorite food is " + buttonCreature.favoriteFood + ".</p>";
     $("#output").html(message);
     console.log(count);
     console.log(arrayPosition);
